21aed04539a0962d886dd4367fc60a5d0fc9628f
12dabbott <damian.d.abbott@gmail.com>
2016-10-12 20:54:05 -0400

    WIP ConwaySequence
    Accidentally made it only work without spaces, doesnt accept answer for
    double digit numbers, which is retarded. TO DO: Make this work with
    spaces and acknowledge double digit numbers

---
+++ Medium Puzzles/ConwaySequence.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+string cs(string answer) {
+	int num = 1, i = 0;
+	answer.erase(remove(answer.begin(), answer.end(), ' '), answer.end());
+	string new_answer;
+	if (answer.length() == 1) {
+		new_answer = "1 " + answer;
+	}
+	else {
+		while (i < answer.length()) {
+			int found = answer.find_first_not_of(answer[i]);
+			if (found != string::npos) {
+				num = abs(found - i);
+				new_answer += to_string(num) + " " + answer[i] + " ";
+				answer.erase(0, found);
+				i = 0;
+			}
+			else {
+				int found = answer.rfind(answer[i]);
+				if (found != string::npos) {
+					num = abs(found - i) + 1;
+					new_answer += to_string(num) + " " + answer[i];
+					answer.clear();
+					i = 0;
+				}
+			}
+		}
+	}
+	return new_answer;
+}
+int main()
+{
+	int R;
+	cin >> R; cin.ignore();
+	int L;
+	cin >> L; cin.ignore();
+	vector<string> answer;
+	string start = to_string(R);
+	answer.push_back(start);
+
+	// Write an action using cout. DON'T FORGET THE "<< endl"
+	// To debug: cerr << "Debug messages..." << endl;
+	for (unsigned int i = 1; i < L; i++) {
+		start = cs(start);
+		answer.push_back(start);
+	}
+	cout << answer[L - 1] << endl;
+}
